5709807e12c31769dbc1f9f30df3db3c1893ad1f
ipolyaolya <ipolyaolya@gmail.com>
2019-08-08 09:39:19 +0300

    New

---
+++ TestTaskTipsMart/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTaskTipsMart
+//
+//  Created by olli on 08.08.19.
+//  Copyright © 2019 Oli Poli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
